bf16a0162e20b7f2484587db778ccdbce86bb4c0
YoelRP <jojoropi@gmail.com>
2017-09-17 18:31:37 -0600

    first state machine

--- bin/DbCPortStateMachine.v
+++ bin/DbCPortStateMachine.v
@@ -16,7 +16,11 @@ input wire PLC   ,
 input wire PRC   ,
 input wire DCE   , 
 input wire PED   ,
-input wire CEC  
+input wire CEC   ,
+input wire Reset_rcvd,
+input wire set_config_succesful,
+input wire EnumError,
+input wire Deconfigure  
 );
 reg [4:0] state;
 
@@ -24,25 +28,94 @@ reg [4:0] state;
 always @ (posedge clock)
 begin 
 if (DCE == 1'b0) begin
-  state = `DBC_OFF; // move to DBC_OFF state 
-  end 	
-
-end
-endmodule
- /*else
-case(state)
-   `DBC_OFF : if (DCE == 1'b1) begin
-                state = `DBC_DISCONNECTED;
-             end
-   `DBC_DISCONNECTED : if (DCE == 1'b0) begin
+  state = `DBC_OFF; 
+end else
+	case(state)
+		`DBC_OFF 		 : if (DCE == 1'b1) begin
+			                state = `DBC_DISCONNECTED;
+			             end else begin 
+                		state = `DBC_OFF;
+                		end 
+	    
+	    `DBC_DISCONNECTED : if (DCE == 1'b0) begin
    						state = `DBC_OFF;
 					end else if (CSC == 1'b1) begin
                 		state = `DBC_ENABLED;
-            		     end else 
-            		    state = `DBC_DISCONNECTED; 
-      default : state = `DBC_OFF;
-  endcase
-end*/
+            		     end else begin
+            		    state = `DBC_DISCONNECTED;  
+            		    	 end 
+
+        `DBC_DISABLED     : if (DCE == 1'b0) begin
+   						state = `DBC_OFF;
+   						end else if (CSC == 1'b1) begin
+                		state = `DBC_DISCONNECTED;
+                		end else if (PED == 1'b1) begin
+                		state = `DBC_ENABLED;
+                		 end else begin
+            		    state = `DBC_DISABLED;  
+            		    	 end 
+
+
+        `DBC_ENABLED 	 : 	 if (DCE == 1'b0) begin
+   						state = `DBC_OFF;
+   						end else if (CSC == 1'b1) begin
+                		state = `DBC_DISCONNECTED;
+                		end else if (PED == 1'b0) begin
+                		state = `DBC_DISABLED;
+   						end else if (CEC == 1'b1 || PLC == 1'b1) begin
+                		state = `DBC_DISABLED;
+                		end else if (EnumError == 1'b1) begin
+                		state = `DBC_ERROR;
+                		end else if (Reset_rcvd == 1'b1) begin
+                		state = `DBC_RESETTING;
+                		end else if (set_config_succesful == 1'b1) begin
+                		state = `DBC_CONFIURED;
+                		end else begin 
+                		state = `DBC_ENABLED;
+                		end 
+        
+        `DBC_RESETTING 	 : 	 if (DCE == 1'b0) begin
+   						state = `DBC_OFF;
+   						end else if (CSC == 1'b1) begin
+                		state = `DBC_DISCONNECTED;
+                		end else if (PED == 1'b0) begin
+                		state = `DBC_DISABLED;
+                		end else if (PRC == 1'b1) begin
+                		state = `DBC_ENABLED;
+                		end  else begin 
+                		state = `DBC_OFF;
+                		end 
+        
+        `DBC_ERROR 		 :	 if (DCE == 1'b0) begin
+   						state = `DBC_OFF;
+   						end else if (CSC == 1'b1) begin
+                		state = `DBC_DISCONNECTED;
+                		end else if (PED == 1'b0) begin
+                		state = `DBC_DISABLED;
+                		end else if (Reset_rcvd == 1'b1) begin
+                		state = `DBC_RESETTING;
+                		end else begin 
+                		state = `DBC_ERROR;
+                		end  
+        `DBC_CONFIURED   : if (DCE == 1'b0) begin
+   						state = `DBC_OFF;
+   						end else if (CSC == 1'b1) begin
+                		state = `DBC_DISCONNECTED;
+                		end else if (PED == 1'b0) begin
+                		state = `DBC_DISABLED;
+                		end else if (Reset_rcvd == 1'b1) begin
+                		state = `DBC_RESETTING;
+                		end else if (Deconfigure == 1'b1) begin
+                		state = `DBC_ENABLED;
+                		end else begin 
+                		state = `DBC_CONFIURED; 
+                		end
+
+
+      default : state = `DBC_OFF;          
+	endcase             
+end
+endmodule
 
 /*`define DBC_DISABLED 5'b11000	
 `define DBC_OFF   5'b00000	
--- bin/DbCPortStateMachine_tb.v
+++ bin/DbCPortStateMachine_tb.v
@@ -10,6 +10,11 @@ module tester;
   reg PED_reg   = 0;
   reg CEC_reg   = 0;
   reg clk = 0;
+  reg Reset_rcvd_reg = 0 ;
+  reg set_config_succesful_reg = 0; 
+ reg EnumError_reg = 0 ;   
+ reg Deconfigure_reg = 0 ; 
+
   initial begin
      $dumpfile("tester.vcd");
      $dumpvars(0,tester);
@@ -26,13 +31,17 @@ module tester;
 
 
 DbCPortStatMachine DbCPortStatMachine_instance ( 
-.clock (  clk     ),
-.DCI   (  DCI_reg ),
-.CSC   (  CSC_reg ),
-.PLC   (  PLC_reg ),
-.PRC   (  PRC_reg ),
-.DCE   (  DCE_reg ), 
-.PED   (  PED_reg ),
-.CEC   (  CEC_reg )
+.clock                   (  clk                       ),
+.DCI                     (  DCI_reg                   ),
+.CSC                     (  CSC_reg                   ),
+.PLC                     (  PLC_reg                   ),
+.PRC                     (  PRC_reg                   ),
+.DCE                     (  DCE_reg                   ), 
+.PED                     (  PED_reg                   ),
+.CEC                     (  CEC_reg                   ),
+.Reset_rcvd              (  Reset_rcvd_reg            ),
+.set_config_succesful    (  set_config_succesful_reg  ),
+.EnumError               (  EnumError_reg             ),
+.Deconfigure             (  Deconfigure_reg           )
 );
 endmodule // test
