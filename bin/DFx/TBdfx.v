26cce7d3da56b1dc1197b1d3169babc53f5668e5
YoelRP <jojoropi@gmail.com>
2018-07-24 03:41:47 -0600

    correcionDFXdone2

---
+++ bin/DFx/TBdfx.v
@@ -0,0 +1,70 @@
+module tester;
+
+reg rclk = 0;
+always #1 rclk = rclk;
+
+  initial begin
+     $dumpfile("tester.vcd");
+     $dumpvars(0,tester);
+     # 513 $finish;
+  end
+
+
+ FF_scan FF_scan_intance1(
+  clk,
+  SE,
+  SD,
+  D,
+  Q
+  );
+ FF_scan FF_scan_intance2(
+  clk,
+  SE,
+  SD,
+  D,
+  Q
+  );
+ FF_scan FF_scan_intance3(
+  clk,
+  SE,
+  SD,
+  D,
+  Q
+  ); 
+ FF_scan FF_scan_intance4(
+  clk,
+  SE,
+  SD,
+  D,
+  Q
+  );
+
+ FF_scan FF_scan_intance5(
+  clk,
+  SE,
+  SD,
+  D,
+  Q
+  );
+ FF_scan FF_scan_intance6(
+  clk,
+  SE,
+  SD,
+  D,
+  Q
+  );
+ FF_scan FF_scan_intance7(
+  clk,
+  SE,
+  SD,
+  D,
+  Q
+  );
+
+  FF_scan FF_scan_intance8(
+  clk,
+  SE,
+  SD,
+  D,
+  Q
+  );
